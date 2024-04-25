86f9c79d8801950477e5bf1e64e26664f1f80529
Brian Alan Suarez <suarezbrianalan@gmail.com>
2024-04-25 20:09:45 -0300

    add firestore, auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"tp1-labo4-suarezbrianalan","appId":"1:583053500880:web:0e30a256770448de8259d5","storageBucket":"tp1-labo4-suarezbrianalan.appspot.com","apiKey":"AIzaSyDprZW9SQ4HWSABJKTNRcXCh9NHjKuhkpc","authDomain":"tp1-labo4-suarezbrianalan.firebaseapp.com","messagingSenderId":"583053500880","measurementId":"G-S8SCWX5NBD"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
